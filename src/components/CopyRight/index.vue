259b7331fa0a04bb2a0a351e664ae1bf3f8ce3f5
ccweng <wengx86@163.com>
2024-11-02 01:36:12 +0800

    feat: 版权信息展示优化

--- src/components/CopyRight/index.vue
+++ src/components/CopyRight/index.vue
@@ -3,9 +3,8 @@
     本文为 <a :href="theme.actions?.github">ccweng</a> 原创文章，遵循
     <a href="https://creativecommons.org/licenses/by-sa/4.0/deed.zh-hans">
       CC 4.0 BY-SA </a
-    >版权协议，转载请附上原文出处链接及本声明。原文链接：<a :href="address">{{
-      address
-    }}</a>
+    >版权协议，转载请附上原文出处链接及本声明。<br />
+    原文链接：<a :href="address">{{ address }}</a>
   </div>
 </template>
 
