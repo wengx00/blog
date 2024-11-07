3e99cb8302dd80514c7596cc2b5ae2d5f087378c
ccweng <wengx86@163.com>
2024-11-07 23:40:00 +0800

    feat: 文档SEO优化

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -8,6 +8,7 @@ import { getFileTimeInfo } from './utils';
 export default {
   title: "CC's Blog",
   description: 'Share tech and life.',
+  lang: 'zh-CN',
   base: '/',
   head: [
     [
@@ -15,6 +16,14 @@ export default {
       {
         name: 'viewport',
         content: 'width=device-width, initial-scale=1.0, viewport-fit=cover',
+      },
+    ],
+    [
+      'meta',
+      {
+        name: 'keywords',
+        content:
+          'Web,前端,JavaScript,Vue,React,Node.js,TypeScript,开发者,程序员,front-end,developer,programmer,科技,生活,技术,docker,linux,macOS,windows,技术博客,flutter,C++,swiftui,xcode,shell,bash,http,https,tcp,操作系统,数据结构,计算机网络,编译原理',
       },
     ],
   ],
