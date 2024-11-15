188aa61ee08b247f565216cb521f94a3de9de713
ccweng <ccweng@qq.com>
2024-11-15 12:06:35 +0800

    chore: 新增百度站点校验头

--- docs/.vitepress/configs/head.ts
+++ docs/.vitepress/configs/head.ts
@@ -14,4 +14,11 @@ export default [
         'ccweng,博客,Web,前端,JavaScript,Vue,React,Node.js,TypeScript,开发者,程序员,front-end,developer,programmer,科技,生活,技术,docker,linux,macOS,windows,技术博客,flutter,C++,swiftui,xcode,shell,bash,http,https,tcp,操作系统,数据结构,计算机网络,编译原理',
     },
   ],
+  [
+    'meta',
+    {
+      name: 'baidu-site-verification',
+      content: 'codeva-zqZt8oKCIH',
+    },
+  ],
 ];
